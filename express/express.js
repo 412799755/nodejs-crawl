872bdd6dd9742457a921d71440b61209f486c850
412799755 <412799755@qq.com>
2019-06-29 17:03:23 +0800

    urlencoded

--- express/express.js
+++ express/express.js
@@ -1,36 +1,35 @@
 var express = require('express')
 var app = express()
 var path = require('path');
-
+app.use(express.json({
+    inflate:true,
+    limit:'100kb',
+    reviver:null,
+    strict:true,
+    type:'application/json',
+    verify:undefined
+}))
+app.use(express.urlencoded({ extended: false }));
 //  主页输出 "Hello World"
 app.get('/', function (req, res) {
     console.log("主页 GET 请求");
     res.send('Hello GET');
 })
-
-
-//  POST 请求
-app.post('/', function (req, res) {
-    console.log("主页 POST 请求");
-    res.send('Hello POST');
-})
-
-//  /del_user 页面响应
-app.get('/del_user', function (req, res) {
-    console.log("/del_user 响应 DELETE 请求");
-    res.send('删除页面');
-})
-
-//  /list_user 页面 GET 请求
-app.get('/list_user', function (req, res) {
-    console.log("/list_user GET 请求");
-    res.send('用户列表页面');
-})
-
 // 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
 app.get('/ab*cd', function(req, res) {
     console.log("/ab*cd GET 请求");
     res.send('正则匹配');
+})
+
+app.post('/process_post', function (req, res) {
+    console.log(req.body)
+    // // 输出 JSON 格式
+    var response = {
+        "first_name":req.body.first_name,
+        "last_name":req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 //静态资源
 var options = {
@@ -53,12 +52,4 @@ var server = app.listen(8081, function () {
 
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
-})
-express.json({
-    inflate:true,
-    limit:'100kb',
-    reviver:null,
-    strict:true,
-    type:'application/json',
-    verify:undefined
 })
