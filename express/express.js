299064d1955ed7d3a5c990b9bb882f9771d27a16
412799755 <412799755@qq.com>
2019-07-02 14:05:21 +0800

    multer 上传文件

--- express/express.js
+++ express/express.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
+var fs = require("fs");
 var path = require('path');
+var multer  = require('multer');
+
 app.use(express.json({
     inflate:true,
     limit:'100kb',
@@ -10,6 +13,7 @@ app.use(express.json({
     verify:undefined
 }))
 app.use(express.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
 //  主页输出 "Hello World"
 app.get('/', function (req, res) {
     console.log("主页 GET 请求");
@@ -20,6 +24,7 @@ app.get('/ab*cd', function(req, res) {
     console.log("/ab*cd GET 请求");
     res.send('正则匹配');
 })
+//post
 app.post('/process_post', function (req, res) {
     console.log(req.body)
     // // 输出 JSON 格式
@@ -36,6 +41,28 @@ app.post('/process_post', function (req, res) {
     // res.end(JSON.stringify('fwa'));
     res.end(JSON.stringify(response));
 })
+//文件上传
+app.post('/file_upload', function (req, res) {
+
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = __dirname + "/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+            }else{
+               var  response = {
+                    message:'File uploaded successfully',
+                    filename:req.files[0].originalname
+                };
+            }
+            console.log( response );
+            res.end( JSON.stringify( response ) );
+        });
+    });
+})
+
 //静态资源
 var options = {
     dotfiles: 'ignore',
@@ -50,7 +77,8 @@ var options = {
 }
 app.use('/public', express.static(path.join(__dirname, '../public')));
 
-var server = app.listen(8081, function () {
+
+var server = app.listen(8081,'172.18.0.113', function () {
 
     var host = server.address().address
     var port = server.address().port
