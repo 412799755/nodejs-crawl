1add921bc2da85090237bebe48683b7527e657c2
412799755 <412799755@qq.com>
2019-07-09 18:02:24 +0800

    get method mock

--- easyMock/list.js
+++ easyMock/list.js
@@ -41,3 +41,43 @@ app.get('/list', function (req, res) {
     // res.send(JSON.stringify(response));
     res.send(JSON.stringify(response));
 })
+
+app.get('/newTask/getClassList', function (req, res) {
+    console.log(req.body)
+    // // 输出 JSON 格式
+    var response = {
+        'data':[{ name: '501班',id:1 }, { name: '502班',id:2 },{ name: '501班',id:3 }, { name: '502班',id:4 },],
+        'code':0,
+        status:'0'
+    };
+    console.log(response,JSON.stringify(response));
+    res.set({
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'POST,GET',
+        'Access-Control-Allow-Headers':'x-requested-with,content-type'
+    })
+    // res.end(JSON.stringify('fwa'));
+    // res.end(JSON.stringify(response));
+    // res.send(JSON.stringify(response));
+    res.send(JSON.stringify(response));
+})
+
+app.get('/newTask/getPeopleByClassId', function (req, res) {
+    console.log(req.query.id)
+    // // 输出 JSON 格式
+    let result = [[{ name: '张三', id: 1}, { name: '李四', id: 2 }],[{ name: '张分割', id: 3}, { name: '李四', id: 4 }]
+        ,[{ name: 'gw ', id: 5}, { name: '李四', id: 6 }]  ,[{ name: '福娃 ', id: 7}, { name: '李四', id: 8 }]
+    ]
+    var response = {
+        'data':result[+req.query.id-1],
+        'code':0,
+        status:'0'
+    };
+    // console.log(response,JSON.stringify(response));
+    res.set({
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'POST,GET',
+        'Access-Control-Allow-Headers':'x-requested-with,content-type'
+    })
+    res.send(JSON.stringify(response));
+})
